f77bc8d8e83211213f75630b69c17687422d5e95
Angelica Zamer <angelica.zamer@gmail.com>
2019-09-16 08:56:38 -0500

    question/answer added to object in js

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+//variables 
+var score = 0;
+
+//start button
+var start = function (){
+    //countdown timer begins
+    //questions are displayed
+}
+
+
+//function for questions & answers
+var questions = [
+    {question: "Which Weasley died in the battle of Hogwarts in Deathly Hallows?",
+    answer: "George" }
+    {question: "Luna Lovegood was introduced in the book _______."
+answer: "Order of the Phoenix" }
+];
+
+
+//results
