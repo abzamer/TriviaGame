4c93c374990d3cc3c9323ddfba5b8327678619db
Angelica Zamer <angelica.zamer@gmail.com>
2019-09-16 23:34:39 -0500

    questions & choices displayed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@ var score = 0;
 var displayGame;
 
 //start button
-// $("#start").click(startQuiz);
+$("#start").click(startQuiz);
 // $("#submit").click(submitQuiz);
 
  //quizQuestions
@@ -19,17 +19,35 @@ var quizQuestions = [
 ];
 
 //function to display questions
-function displayQuestion(quizQuestion, index){
-    $("#quiz").innerHTML = quizQuestion.question;
+// function displayQuestion(quizQuestion, index){
+//     $("#quiz").html(quizQuestion);
+//     console.log(quizQuestions);
+// }
+
+//function to display question
+function showQuestions(quizQuestions){
+    $("#quiz").html(quizQuestions[0].question + "<br>" + 
+    "<input type=\"radio\" name=\"q1\" value="+quizQuestions[0].choices[0] +"> "+ quizQuestions[0].choices[0] +
+    "<input type=\"radio\" name=\"q1\" value="+quizQuestions[0].choices[1] +"> "+ quizQuestions[0].choices[1] +
+    "<input type=\"radio\" name=\"q1\" value="+quizQuestions[0].choices[2] +"> "+ quizQuestions[0].choices[2] +
+    "<input type=\"radio\" name=\"q1\" value="+quizQuestions[0].choices[3] +"> "+ quizQuestions[0].choices[3] +
+    "<br>" +
+    quizQuestions[1].question + "<br>" +
+    "<input type=\"radio\" name=\"q2\" value="+quizQuestions[1].choices[0] +"> "+ quizQuestions[1].choices[0] +
+    "<input type=\"radio\" name=\"q2\" value="+quizQuestions[1].choices[1] +"> "+ quizQuestions[1].choices[1] +
+    "<input type=\"radio\" name=\"q2\" value="+quizQuestions[1].choices[2] +"> "+ quizQuestions[1].choices[2] +
+    "<input type=\"radio\" name=\"q2\" value="+quizQuestions[1].choices[3] +"> "+ quizQuestions[1].choices[3]);
 }
 
 //function startQuiz
 function startQuiz(){
     displayGame = [];
-    quizQuestions.forEach(displayQuestion)
+    showQuestions(quizQuestions);
+    // quizQuestions.forEach(displayQuestion)
  }
+ 
 
-// for loop to
+
 
 
 
